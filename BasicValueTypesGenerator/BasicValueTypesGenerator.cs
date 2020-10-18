69ca01d1a1f5d44f3c156527a54a75d080b6ed4a
arinab123@gmail.com <arinab123@gmail.com>
2020-10-18 16:55:12 +0300

    Change long, ulong and decimal generation

--- BasicValueTypesGenerator/BasicValueTypesGenerator.cs
+++ BasicValueTypesGenerator/BasicValueTypesGenerator.cs
@@ -7,6 +7,13 @@ namespace BasicValueTypesGenerator
     public class BasicValueTypesGenerator : IGenerator
     {
         private Random random = new Random();
+        long LongRandom(long min, long max, Random rand)
+        {
+            byte[] buf = new byte[8];
+            rand.NextBytes(buf);
+            long longRand = BitConverter.ToInt64(buf, 0);
+            return (Math.Abs(longRand % (max - min)) + min);
+        }
         private Type[] possibleTypes =
             {
                 typeof(byte),
@@ -25,10 +32,7 @@ namespace BasicValueTypesGenerator
         };
         public bool CanGenerate(Type type)
         {
-            if (possibleTypes.Contains(type))
-                return true;
-            else
-                return false;
+            return possibleTypes.Contains(type);
         }
 
         public object Generate(Type type)
@@ -40,13 +44,13 @@ namespace BasicValueTypesGenerator
                 case TypeCode.Char: return (char)random.Next('A', 'z');
                 case TypeCode.Int16: return (short)random.Next();
                 case TypeCode.UInt16: return (ushort)random.Next();
-                case TypeCode.Int32: return (int)random.Next();
+                case TypeCode.Int32: return random.Next();
                 case TypeCode.UInt32: return (uint)random.Next();
-                case TypeCode.Int64: return (long)random.Next();
-                case TypeCode.UInt64: return (ulong)random.Next();
+                case TypeCode.Int64: return LongRandom(long.MinValue, long.MaxValue, random);
+                case TypeCode.UInt64: return (ulong)LongRandom(long.MinValue, long.MaxValue, random);
                 case TypeCode.Single: return (float)random.NextDouble();
                 case TypeCode.Double: return (double)random.NextDouble();
-                case TypeCode.Decimal: return (decimal)random.NextDouble();
+                case TypeCode.Decimal: return new decimal(random.Next(), random.Next(), random.Next(0x204FCE5E), false, 0); 
                 case TypeCode.SByte: return (sbyte)random.Next();
                 default: return null;
             }
---
+++ StringGenerator/StringGenerator.cs
@@ -0,0 +1,18 @@
+﻿using GeneratorBase;
+using System;
+
+namespace StringGenerator
+{
+    public class StringGenerator : IGenerator
+    {
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(string);
+        }
+
+        public object Generate(Type type)
+        {
+            
+        }
+    }
+}
